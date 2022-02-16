1bfc5d3599f5116623ab488c8bed81f5e652c17f
ianpye <ianpangye@gmail.com>
2022-02-16 17:04:11 +0800

    Update model.py
    Added function of choosing predictions made using confidence level and mapping predicted image name to image id

--- model.py
+++ model.py
@@ -1,26 +1,81 @@
-import cv2
 import torch
-import os
 from PIL import Image
-import time
-import pandas
+from imutils import paths
 
 
+# Initializes the model at the start, run model = load_model
 def load_model():
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s_best.pt')
+    model = torch.hub.load('./', 'custom', path='yolov5s_best.pt', source='local')
     return model
 
-
-def predict_image(image, model):
-    img = Image.open(os.path.join('uploads', image))
+# Takes in the image from the 'test' folder, and outputs the predicted label - sample at the end
+# Images with predicted bounding boxes are saved in the runs folder
+def predict_image(image,model):
+    img = Image.open('test/' + image)
     results = model(img)
-    pred_list = results.pandas().xyxy[0]['name'].to_numpy()
+    results.save('runs')
+    df_results = results.pandas().xyxy[0].sort_values('confidence', ascending=True)
+    pred_list = df_results['name'].to_numpy()
     pred = 'NA'
     for i in pred_list:
         if i != 'Bullseye':
             pred = i
-    return pred
+    name_to_id = {
+        "One":      11,
+        "Two":      12,
+        "Three":    13,
+        "Four":     14,
+        "Five":     15,
+        "Six":      16,
+        "Seven":    17,
+        "Eight":    18,
+        "Nine":     19,
+        "A":        20,
+        "B":        21,
+        "C":        22,
+        "D":        23,
+        "E":        24,
+        "F":        25,
+        "G":        26,
+        "H":        27,
+        "S":        28,
+        "T":        29,
+        "U":        30,
+        "V":        31,
+        "W":        32,
+        "X":        33,
+        "Y":        34,
+        "Z":        35,
+        "Up":       36,
+        "Down":     37,
+        "Right":    38,
+        "Left":     39,
+        "Stop":     40
+    }
+    image_id = str(name_to_id[pred])
+    return image_id
 
+# Stitches the previously predicted images in the folder together and saves it into runs/stitched folder
+# This function can be called by itself
+def stitch_image():
+    imgFolder = 'runs'
+    newPath = 'runs/stitched.jpg'
+    imgPath = list(paths.list_images(imgFolder))
+    images = [Image.open(x) for x in imgPath]
+    width, height = zip(*(i.size for i in images))
+    total_width = sum(width)
+    max_height = max(height)
+    stitchedImg = Image.new('RGB', (total_width, max_height))
+    x_offset = 0
+    for im in images:
+        stitchedImg.paste(im, (x_offset,0))
+        x_offset += im.size[0]
+    stitchedImg.save(newPath)
+
+## Load
 # model = load_model()
+
+## Predict
 # image = '2_1.jpg'
-# print(predict_image(image,model))
+# print(predict_image(image, model))
+
